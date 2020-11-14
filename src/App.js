1af709dd9c9e4f071171dac29692adf4d4a43b0c
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-13 20:28:58 -0800

    added Center

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Center from './components/Center/Center';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Center />
       <img src='https://live.staticflickr.com/65535/50598577703_9ccb175dd3_b.jpg' alt="site" />
     </div>
   );
---
+++ src/components/Center/Center.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Center.css';
+import introPic from '../../assets/images/cleaning.jpg';
+
+
+const Center = () => {
+    return (
+        <img className='intro-pic' src={introPic} alt=""/>
+    );
+};
+
+export default Center;
