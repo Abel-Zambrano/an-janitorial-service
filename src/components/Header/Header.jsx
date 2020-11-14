a78d4d9a71e6b8f52dcdff2abc2571fb0bf95014
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-13 19:16:38 -0800

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1 style={{textAlign: 'center'}}>AN Janitorial Service</h1>
+        </header>
+    );
+};
+
+export default Header;
