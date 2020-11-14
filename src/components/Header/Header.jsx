a4e6f4be7f0b9f3b7da9b89a78b1f4d7e8b1eefc
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-13 19:37:05 -0800

    update to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
         <header>
-            <h1 style={{textAlign: 'center'}}>AN Janitorial Service</h1>
+            <nav>
+                <h1>AN Janitorial Service</h1>
+            </nav>
         </header>
     );
 };
