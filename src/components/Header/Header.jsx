b8c3fabe25384cc8db5d533f439f0379e7d4aff2
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-14 11:45:46 -0800

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,17 +3,15 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className='bg-img'>
-            <nav className='navigation'>
-                <h1>AN Janitorial Service</h1>
-                <ul className='nav-links'>
-                    <li>HOME</li>     
-                    <li>ABOUT US</li>     
-                    <li>SERVICES</li>     
-                    <li><i className="fas fa-phone-alt"></i> (510)913-5091</li>         
-                </ul>
-            </nav>
-        </div>
+        <nav className='navigation'>
+            <h1>AN Janitorial Service</h1>
+            <ul className='nav-links'>
+                <li>HOME</li>     
+                <li>ABOUT</li>     
+                <li>SERVICES</li>          
+                <li><i className="fas fa-phone-alt"></i> (510)913-5091</li>         
+            </ul>
+        </nav>
     );
 };
 
