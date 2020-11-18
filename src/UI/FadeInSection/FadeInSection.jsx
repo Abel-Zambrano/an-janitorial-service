13f036aed7830328e046356305d06e7baa21226a
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-17 20:16:59 -0800

    added animation to services section

--- src/UI/FadeInSection/FadeInSection.jsx
+++ src/UI/FadeInSection/FadeInSection.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
+import './FadeInSection.css';
 
 function FadeInSection(props) {
     const [ isVisible, setIsVisible ] = useState(false);
