a4013a3e5463583af94df79c836a906a7e4e07df
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-18 19:25:16 -0800

    added link to phone number

--- src/components/Header/Title/Title.jsx
+++ src/components/Header/Title/Title.jsx
@@ -4,7 +4,9 @@ import './Title.css';
 const Title = () => {
     return (
         <div className='title-div'>
-            <h1 className='Title'>AN Janitorial Service | <i id='phone' className="fas fa-phone-alt"></i> (510) 913-5091</h1>
+            <h1 className='Title'>AN Janitorial Service | 
+            <a className="phone-number" href="tel:5109135091"> <i id='phone' className="fas fa-phone-alt"></i> (510) 913-5091</a>         
+            </h1>
         </div>
     );
 };
