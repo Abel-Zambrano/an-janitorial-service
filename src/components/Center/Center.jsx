c4fafda1140b2a9f53b6d874f11fb666fa90996c
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-21 15:23:09 -0800

    update

--- src/components/Center/Center.jsx
+++ src/components/Center/Center.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Center.css';
 import office from '../../assets/images/bg-an.jpeg';
-import { Parallax } from 'react-parallax';
+
 
 
 const Center = () => {
     return (
         <div>
-            <Parallax className='bg-pic' bgImage={office} strength={500}>
+            <div className='bg-pic'>
                 <div className='bg-info'><i class="fas fa-map-marker-alt"></i> SF Bay Area</div>
-            </Parallax>
+            </div>
         </div>    
     );
 };
