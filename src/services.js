1fe646f72cad33afbc1375f5cb15ba88971e8cb1
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-21 13:56:27 -0800

    update

--- src/services.js
+++ src/services.js
@@ -3,37 +3,37 @@ const services = [
         id: 1,
         name: 'Cleaning',
         img: 'https://live.staticflickr.com/65535/50607993817_7e9303afd5_o.png',
-        description: 'Commercial Cleaning'
+        description: 'COMMERCIAL CLEANING'
     },
     {
         id: 2,
         name: 'Window Cleaning',
         img: 'https://live.staticflickr.com/65535/50607993687_1fee75c445_o.png',
-        description: 'Window Cleaning'
+        description: 'WINDOW CLEANING'
     },
     {
         id: 3,
         name: 'Carpet Cleaning',
         img: 'https://live.staticflickr.com/65535/50607993702_cf6a45fd86_o.png',
-        description: 'Carpet Cleaning'
+        description: 'CARPET CLEANING'
     },
     {
         id: 4,
         name: 'Wax Floor',
         img: 'https://live.staticflickr.com/65535/50607876931_fefde1d36b_o.png',
-        description: 'Floor Waxing and Buffing'
+        description: 'FLOOR BUFFING'
     },
     {
         id: 5,
         name: 'Garbage Disposal',
         img: 'https://live.staticflickr.com/65535/50607129403_83f4fbeb2c_o.png',
-        description: 'Garbage Disposal'
+        description: 'GARBAGE DISPOSAL'
     },
     {
         id: 6,
         name: 'Pressure Washing',
         img: 'https://live.staticflickr.com/65535/50607876956_f388cac25d_o.png',
-        description: 'Pressure Washing'
+        description: 'PRESSURE WASHING'
     }
 ]
 
