a7bc85204a62558aab18a35f71174ab3f6d4b530
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-20 21:27:10 -0800

    update

--- src/components/Center/Center.jsx
+++ src/components/Center/Center.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Center.css';
-import office from '../../assets/images/office.jpg';
+import office from '../../assets/images/bg-an.jpeg';
 import { Parallax } from 'react-parallax';
 
 
 const Center = () => {
     return (
         <div>
-            <Parallax className='bg-pic' bgImage={office} strength={1200}>
+            <Parallax className='bg-pic' bgImage={office} strength={500}>
                 <div className='bg-info'><i class="fas fa-map-marker-alt"></i> SF Bay Area</div>
             </Parallax>
         </div>    
