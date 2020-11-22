f6ca0068be61f6628c6f07033ef5b929e4daad50
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-22 12:27:27 -0800

    update

--- src/UI/BouncingArrow/BouncingArrow.jsx
+++ src/UI/BouncingArrow/BouncingArrow.jsx
@@ -3,7 +3,7 @@ import './BouncingArrow.css';
 
 function BouncingArrow() {
     return (
-        <div class="arrow bounce"><i class="fa fa-angle-down fa-5x" aria-hidden="true"></i></div>
+        <div className="arrow bounce"><i className="fa fa-angle-down fa-5x" aria-hidden="true"></i></div>
     );
 };
 
