01422e205a96fa3d34c8996070cc5d55b36a6e48
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-21 22:22:55 -0800

    added bouncing arrow

---
+++ src/UI/BouncingArrow/BouncingArrow.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './BouncingArrow.css';
+
+function BouncingArrow() {
+    return (
+        <div class="arrow bounce"><i class="fa fa-angle-down fa-5x" aria-hidden="true"></i></div>
+    );
+};
+
+export default BouncingArrow;
--- src/components/Center/Center.jsx
+++ src/components/Center/Center.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Center.css';
+import BouncingArrow from '../../UI/BouncingArrow/BouncingArrow';
 
 const Center = () => {
     return (
         <div>
             <div className='bg-pic'>
                 <div className='bg-info'><i class="fas fa-map-marker-alt"></i> SF Bay Area</div>
+                <BouncingArrow />
             </div>
         </div>    
     );
