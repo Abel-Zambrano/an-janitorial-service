eaed8639fa503f2f41e49a0b56ab269ed6c36e96
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-12-14 19:23:17 -0800

    update

--- src/components/Logo/Logo/Logo.jsx
+++ src/components/Logo/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import './Logo.css';
 const Logo = () => {
     return (
         <div className='LogoDiv'>
-            <img className='LogoImg' src="https://live.staticflickr.com/65535/50716942948_1b27ae05b3_o.png" alt="an janitorial"/>
+            <img className='LogoImg' src="https://live.staticflickr.com/65535/50721630991_3f20ec58be_o.png" alt="an janitorial"/>
         </div>
     );
 };
