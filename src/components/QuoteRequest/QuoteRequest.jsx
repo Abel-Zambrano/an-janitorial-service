4fa59e18e1b75769a6b4a6e44ac82307a43817b7
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-12-15 22:03:42 -0800

    fixed quote button

--- src/components/QuoteRequest/QuoteRequest.jsx
+++ src/components/QuoteRequest/QuoteRequest.jsx
@@ -4,10 +4,9 @@ import './QuoteRequest.css';
 const QuoteRequest = () => {
     return (
         <div className='QuoteDiv'>
-            <div className='Quote'>
-                <h2><i className="fas fa-concierge-bell"></i></h2>
-                <h2>Request a Quote</h2>
-            </div>
+            <button className='Quote'>
+                <h2><i className="fas fa-concierge-bell"></i> Request a Quote</h2>
+            </button>
         </div>
     );
 };
