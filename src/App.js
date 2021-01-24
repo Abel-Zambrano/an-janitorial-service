2ef87c940a77eb1fb39ccb6a1834ab2659f4407c
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-01-23 20:31:20 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import Center from './components/Center/Center';
