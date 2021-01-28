6d68337f84a5ac03507e77673523e3bb5cc0d213
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-01-27 20:45:36 -0800

    update to modal

--- src/components/EmailForm/EmailForm.jsx
+++ src/components/EmailForm/EmailForm.jsx
@@ -3,22 +3,22 @@ import './EmailForm.scss';
 
 const EmailForm = (props) => {
     return (
-        <div className='emailForm-div'>
-            <div className="cancel-btn">
+        <div className='EmailForm'>
+            <div className="EmailForm__cancel-btn">
                 <button onClick={props.clicked} className='danger'>X</button>
             </div>
             <h2>Contact us for a Quote!</h2>
-            <form className='quote-form' onSubmit={props.submit}>
-                <div className="input-fields">
-                    <input className='input' type="text" placeholder='Name' name='name' required/>
-                    <input className='input' type="tel" pattern='[0-9]{10}' placeholder='Phone Number' name='phone number' required/>
-                    <input className='input' type="email" placeholder='Email' name='email' />
+            <form className='EmailForm__quote' onSubmit={props.submit}>
+                <div className="EmailForm__input-fields">
+                    <input className='EmailForm__input-fields-input' type="text" placeholder='Name' name='name' required/>
+                    <input className='EmailForm__input-fields-input' type="tel" pattern='[0-9]{10}' placeholder='Phone Number' name='phone number' required/>
+                    <input className='EmailForm__input-fields-input' type="email" placeholder='Email' name='email' />
                 </div>
-                <div className="textarea-field">
-                    <textarea className='textarea' name="message" cols="30" rows="10" placeholder='Message' required></textarea>
+                <div className="EmailForm__text-area">
+                    <textarea className='EmailForm__text-area-text' name="message" cols="30" rows="10" placeholder='Message' required></textarea>
                 </div>
-                <div className="btn-div">
-                    <button type='submit' onClick={props.sent} className='success'>SEND</button>
+                <div className="EmailForm__send-btn">
+                    <button type='submit' onClick={props.sent} className='EmailForm__send-btn-success'>SEND</button>
                 </div>
             </form>
         </div>
