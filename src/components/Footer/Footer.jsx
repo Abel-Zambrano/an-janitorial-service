a85140d4fb4430b578621c820ab5981a5d6be3c0
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-01-28 19:51:58 -0800

    update to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.scss';
 
 function Footer() {
-return <footer className='copyright'><i className="far fa-copyright"></i> Abel Zambrano 2020</footer>
+return <footer className='copyright'><i className="far fa-copyright"></i> Abel Zambrano 2021</footer>
 }
 
 export default Footer;
