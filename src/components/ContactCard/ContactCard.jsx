b959d412e8d75ae1cd8228adbd1c76e0c3234dca
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-01-28 19:08:19 -0800

    update

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -3,5 +3,16 @@ import 'ContactCard.scss';
 
 const ContactCard = () => {
     return (
+        <div className="ContactCard">
+            <h2 className="ContactCard__text">Contact</h2>
+            <div className="ContactCard__info">
+                <p className="ContactCard__info-name">Chris Mendoza | Owner</p>
+                <p className="ContactCard__info-phone-number">
+                    <a href="tel:5109135091"> <i id='phone' className="fas fa-phone-alt"></i> (510) 913-5091</a>
+                </p>
+            </div>
+        </div>
     );
 };
+
+export default ContactCard;
