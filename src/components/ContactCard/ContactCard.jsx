638da74ea67bf9526bc1e5049a05669d26fc19b7
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-01-28 19:47:57 -0800

    update

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -8,7 +8,7 @@ const ContactCard = () => {
             <div className="ContactCard__info">
                 <p className="ContactCard__info-name">Chris Mendoza | Owner</p>
                 <p className="ContactCard__info-phone-number">
-                    <a href="tel:5109135091"> <i id='phone' className="fas fa-phone-alt"></i> (510) 913-5091</a>
+                    <a href="tel:5109135091">(510) 913-5091</a>
                 </p>
             </div>
         </div>
