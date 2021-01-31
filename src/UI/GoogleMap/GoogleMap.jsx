00bc905f7c04913078bb93d74fe0b7a1eca20834
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-01-31 15:37:47 -0800

    update

--- src/UI/GoogleMap/GoogleMap.jsx
+++ src/UI/GoogleMap/GoogleMap.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const GoogleMap = (props) => {
-
     return (
       <img className={props.styleName} 
       src= {`https://maps.googleapis.com/maps/api/staticmap?center=SanLeandro&zoom=10&size=600x300&maptype=roadmap&key=${process.env.REACT_APP_MAP}`}
       alt="map" />  
     );
-
 };
 
 export default GoogleMap;
