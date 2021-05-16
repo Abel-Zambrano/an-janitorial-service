a0b9b70144ce01494221eb77f365fd4c53a34cce
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-05-16 12:53:01 -0700

    removed google map for stability reasons

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './ContactCard.scss';
-import GoogleMap from '../../UI/GoogleMap/GoogleMap';
+import React from "react";
+import "./ContactCard.scss";
+// import GoogleMap from '../../UI/GoogleMap/GoogleMap';
 
 const ContactCard = () => {
-    return (
-        <div className="ContactCard">
-            <h2 className="ContactCard__text">Contact</h2>
-            <div className="ContactCard__info">
-                <p className="ContactCard__info-name">Chris Mendoza | Owner</p>
-                <p className="ContactCard__info-phone-number">
-                    <a href="tel:5109135091">(510) 913-5091</a>
-                </p>
-            </div>
-            <div className="map-container">
+  return (
+    <div className="ContactCard">
+      <h2 className="ContactCard__text">Contact</h2>
+      <div className="ContactCard__info">
+        <p className="ContactCard__info-name">Chris Mendoza | Owner</p>
+        <p className="ContactCard__info-phone-number">
+          <a href="tel:5109135091">(510) 913-5091</a>
+        </p>
+      </div>
+      {/* <div className="map-container">
                 <GoogleMap styleName='map' />
-            </div>
-        </div>
-    );
+            </div> */}
+    </div>
+  );
 };
 
 export default ContactCard;
