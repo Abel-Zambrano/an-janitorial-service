79727edf225536935a8f11a3201085d7045c69a3
Abel Zambrano <azambrano1417@gmail.com>
2021-07-31 09:59:21 -0700

    update to logo

--- src/components/Center/Center.jsx
+++ src/components/Center/Center.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import './Center.scss';
-import Logo from '../Logo/Logo/Logo';
-import Banner from '../Banner/Banner';
-import BouncingArrow from '../../UI/BouncingArrow/BouncingArrow';
+import React from "react";
+import "./Center.scss";
+import Logo from "../Logo/Logo/Logo";
+import Banner from "../Banner/Banner";
+import BouncingArrow from "../../UI/BouncingArrow/BouncingArrow";
 
 const Center = () => {
-    return (
-        <div className="Center">
-            <div className="Center__background">
-                <div className="Center__logo-box">
-                    <Logo />
-                </div>
-                <div className="Center__banner-box">
-                    <Banner styleName="Center__banner-text" />
-                </div>
-                <div className="Center__arrow-box">
-                    <BouncingArrow styleName='bouncing-arrow' />
-                </div>
-            </div>
-        </div>   
-    );
+  return (
+    <div className="Center">
+      <div className="Center__background">
+        <div className="Center__logo-container">
+          <div className="Center__logo-box">
+            <Logo />
+          </div>
+        </div>
+        <div className="Center__banner-box">
+          <Banner styleName="Center__banner-text" />
+        </div>
+        <div className="Center__arrow-box">
+          <BouncingArrow styleName="bouncing-arrow" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Center;
